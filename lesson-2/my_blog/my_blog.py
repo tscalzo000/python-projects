e9baf6ed2adc28bcd7fa1ea128dabdb15f5cafaf
vscalzo000 <vscalzo000@gmail.com>
2017-02-19 08:22:01 -0500

    Lesson 2

---
+++ lesson-2/my_blog/my_blog.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return app.send_static_file('home.html')
+
+@app.route('/about')
+def about():
+    return app.send_static_file('about.html')
+
+@app.route('/contact')
+def contact():
+    return app.send_static_file('contact.html')
+
+@app.route('/post/<postnum>')
+def post(postnum):
+    return 'This is post ' + postnum
